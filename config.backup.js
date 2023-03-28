4d920c775c81be3b962ca57ad33b57ac44a50060
wangjitao <jitao.wang@aivics.com>
2023-03-28 23:11:58 +0800

    配置备份

--- config.backup.js
+++ config.backup.js
@@ -1,19 +1,18 @@
-// config.js 文件内容参考
 const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize'
-const SCOPE ='user'
-const client_id = ''
+const SCOPE = 'user'
+const client_id = '10134e9ddcae4187b1cc'
 
 module.exports = {
-  github: {
-    github_base_url: 'https://api.github.com',
-    request_token_url: 'https://github.com/login/oauth/access_token',
-    client_id,
-    client_secret: ''
-  },
-  redis: {
-    port: 0,
-    password: 0
-  },
-  GITHUB_OAUTH_URL,
-  OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${client_id}&scope=${SCOPE}`
+	github: {
+		github_base_url: 'https://api.github.com',
+		request_token_url: 'https://github.com/login/oauth/access_token',
+		client_id,
+		client_secret: '74d77a793c0b1f277910e3adff0875c68c568cbc',
+	},
+	radis: {
+		port: 6379,
+		password: '',
+	},
+	GITHUB_OAUTH_URL,
+	OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${client_id}&scope=${SCOPE}`,
 }
